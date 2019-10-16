417bb3fcd4b523cd4f826483fc3e1c779e52c5a8
Drew Pepin <drewpepin1995@gmail.com>
2019-10-16 19:57:57 -0400

    fixing connecting to db on jawsDB

--- config/database.js
+++ config/database.js
@@ -1,16 +1,23 @@
 const Sequelize = require("sequelize")
-module.exports =
+let db;
+
+if (process.env.NODE_ENV != "production") {
     db = new Sequelize('sports.db', 'root', 'hockey930', {
-        host: 'localhost',
-        dialect: 'mysql',
-        define: {
-            timestamps: false
-        },
+    host: 'localhost',
+    dialect: 'mysql',
+    define: {
+      timestamps: false
+    },
 
-        pool: {
-            max: 5,
-            min: 0,
-            idle: 10000
-        },
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }});
 
-    });
+
+  } else {
+    db = new Sequelize(process.env.JAWSDB_URL);
+  }
+
+module.exports = db;
