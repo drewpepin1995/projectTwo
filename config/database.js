d16a180865ac4873d4daf5f26bfe9ab3e6ec5d83
Julia Cassidy <juliacassidy@Julias-MacBook-Pro-2.local>
2019-10-17 19:25:01 -0400

    pulling

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const Sequelize = require("sequelize")
+<<<<<<< HEAD
 module.exports =
     db = new Sequelize('sports_db', 'root', 'root', {
         host: 'localhost',
@@ -6,11 +8,26 @@ module.exports =
         define: {
             timestamps: false
         },
+=======
+let db;
+if (process.env.NODE_ENV != "production") {
+    db = new Sequelize('sports.db', 'root', 'hockey930', {
+    host: 'localhost',
+    dialect: 'mysql',
+    define: {
+      timestamps: false
+    },
+>>>>>>> 9361c7fefa968aa06af6f6404d609467e2ca42e4
 
-        pool: {
-            max: 5,
-            min: 0,
-            idle: 10000
-        },
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }});
 
-    });
+
+  } else {
+    db = new Sequelize(process.env.JAWSDB_URL);
+  }
+
+module.exports = db;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models/smack');
 
-
-router.get("/", function(req, res) {
-    res.render("index",{ layout: 'landing'})
+router.get("/", function (req, res) {
+    console.log(db);
+    res.render("index", { layout: 'landing' })
 });
+
+router.get('/smack', (req, res) =>
+    db.findAll()
+        .then(smacks => {
+            res.render('smacks', {
+                smacks
+            });
+
+        })
+        .catch(err => console.log("error: " + err)));
+
+
+
 
 module.exports = router
