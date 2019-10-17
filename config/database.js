0a2ed9e42cbe4e5247d5de41ecf48745ea06eefb
Julia Cassidy <juliacassidy@Julias-MacBook-Pro-2.local>
2019-10-17 19:37:26 -0400

    more style

--- config/database.js
+++ config/database.js
@@ -1,14 +1,5 @@
 require('dotenv').config()
 const Sequelize = require("sequelize")
-<<<<<<< HEAD
-module.exports =
-    db = new Sequelize('sports_db', 'root', 'root', {
-        host: 'localhost',
-        dialect: 'mysql',
-        define: {
-            timestamps: false
-        },
-=======
 let db;
 if (process.env.NODE_ENV != "production") {
     db = new Sequelize('sports.db', 'root', 'hockey930', {
@@ -17,7 +8,6 @@ if (process.env.NODE_ENV != "production") {
     define: {
       timestamps: false
     },
->>>>>>> 9361c7fefa968aa06af6f6404d609467e2ca42e4
 
     pool: {
       max: 5,
